446f34d36da078cb1141ba21b29c3d063ee28b8a
Eesa Wazir <eesa.wazir@gourmetpakistan.com>
2025-02-04 18:04:43 +0500

    user password update - update sid expiry to 15 days

--- frappe/auth.py
+++ frappe/auth.py
@@ -353,8 +353,8 @@ class CookieManager:
 		if not frappe.local.session.get("sid"):
 			return
 
-		# sid expires in 3 days
-		expires = datetime.datetime.now() + datetime.timedelta(days=3)
+		# sid expires in 15 days
+		expires = datetime.datetime.now() + datetime.timedelta(days=15)
 		if frappe.session.sid:
 			self.set_cookie("sid", frappe.session.sid, expires=expires, httponly=True)
 		if frappe.session.session_country:
